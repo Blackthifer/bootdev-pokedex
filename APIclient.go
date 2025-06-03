b99ca1b517e5fa3ee4e01bdf7c5cca1b60fded56
Blackthifer <hessel96@msn.com>
2025-06-03 03:38:31 +0200

    adds basic http get usage

---
+++ APIclient.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type config struct{
+	Next string
+	Previous string
+}
+
+func getData(url string) ([]byte, error){
+	res, err := http.Get(url)
+	if err != nil{
+		return nil, fmt.Errorf("Error retrieving data: %w", err)
+	}
+	defer res.Body.Close()
+	data, err := io.ReadAll(res.Body)
+	if err != nil{
+		return nil, fmt.Errorf("Error reading response body: %w", err)
+	}
+	return data, nil
+}
